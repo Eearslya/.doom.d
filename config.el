4c6c2288b88a7ddce7ebc3ac2e923ff46ac2a1fd
Eearslya Sleiarion <eearslya@gmail.com>
2023-07-14 09:49:36 -0700

    Update config

--- config.el
+++ config.el
@@ -3,15 +3,16 @@
 (setq user-full-name "Eearslya Sleiarion"
       user-mail-address "eearslya@gmail.com")
 
-(setq doom-font (font-spec :family "Source Code Pro" :size 14 :weight 'semi-bold)
+(setq doom-font (font-spec :family "Source Code Pro" :size 16 :weight 'semi-bold)
       doom-theme 'doom-one)
 (unless (doom-font-exists-p doom-font)
   (setq doom-font nil))
 
 (setq display-line-numbers-type t
       indent-tabs-mode nil
-      org-directory "~/org/"
-      tab-width 2)
+      org-directory "~/org/")
+
+(setq-default tab-width 2)
 
 (add-to-list 'initial-frame-alist '(fullscreen . maximized))
 (doom/set-indent-width 2)
@@ -28,6 +29,10 @@
   (define-key company-active-map (kbd "<tab>") #'company-complete-selection)
   (define-key company-active-map (kbd "TAB") #'company-complete-selection)
   (define-key company-active-map (kbd "SPC") nil))
+
+(after! debugger
+  (setq dap-auto-configure-mode t)
+  (require 'dap-cpptools))
 
 (after! lsp-clangd
   (set-lsp-priority! 'clangd 2)
@@ -48,4 +53,6 @@
         projectile-project-search-path '("~/Dev" "/Dev"))
   (setq-default projectile-project-configure-cmd "cmake -S . -B Build -DCMAKE_EXPORT_COMPILE_COMMANDS=On -G\"Unix Makefiles\" -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_BUILD_TYPE=Debug"
                 projectile-project-compilation-cmd "cmake --build Build --parallel 8"
-                projectile-project-run-cmd "cmake --build Build --target Run --parallel 8"))
+                projectile-project-run-cmd "cmake --build Build --target Run --parallel 8")
+  (add-to-list 'projectile-globally-ignored-directories "^[Bb]uild$")
+  (add-to-list 'projectile-globally-ignored-directories "^[Ee]xternal$"))
