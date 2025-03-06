02bf00a54087d7836880d5018a7d5ba62d64db2e
Eearslya Sleiarion <eearslya@gmail.com>
2025-03-06 09:17:25 -0800

    Initial commit

---
+++ config.el
@@ -0,0 +1,47 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+;;
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+;;
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets. It is optional.
+;; (setq user-full-name "John Doe"
+;;       user-mail-address "john@doe.com")
+;;
+;; Doom exposes five (optional) variables for controlling fonts in Doom:
+;;
+;; - `doom-font' -- the primary font to use
+;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
+;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;; - `doom-symbol-font' -- for symbols
+;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
+;;
+;; See 'C-h v doom-font' for documentation and more examples of what they
+;; accept. For example:
+;;
+;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
+;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
+;;
+;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
+;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
+;; refresh your font settings. If Emacs still can't find your font, it likely
+;; wasn't installed correctly. Font issues are rarely Doom issues!
+
+(setq user-full-name "Eearslya Sleiarion"
+      user-mail-address "eearslya@gmail.com")
+
+(setq display-line-numbers-type t)
+
+(add-to-list 'initial-frame-alist '(fullscreen . maximized))
+
+(use-package! org-auto-tangle
+  :defer t
+  :hook (org-mode . org-auto-tangle-mode)
+  :config
+  (setq org-auto-tangle-default t))
+
+(setq-hook! c-mode
+  c-basic-offset 2)
+(setq-hook! c++-mode
+  c-basic-offset 2)
